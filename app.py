2e1af048b42f401c2e14ead267fc6df33b330373
sfono96 <sfono96@gmail.com>
2013-08-30 17:15:36 -0600

    adding crt group view

--- app.py
+++ app.py
@@ -37,6 +37,33 @@ def grade_by_crt(crt):
 		data_series.append(dict)
 	return data_series
 
+# method to filter CRT Groups By Grade Level and return relevant assessments
+def crt_by_grade(grade):
+	# data series
+	data_series = []
+	for crt in crt_groups: # each crt group is a series
+		data = [] # this will be one average score per relevant assessment for the respective crt group
+		for a in assessments:
+			if grade == 'all':
+				scores = [float(row['score']) for row in mylist if row['CRT Score Group'] == crt if row['standard'] == a]
+				data.append(average(scores))
+			else:
+				scores = [float(row['score']) for row in mylist if row['CRT Score Group'] == crt if row['standard'] == a if row['grade'] == grade]
+				if sum(scores) > 0:
+					data.append(average(scores))
+		dict = {}
+		dict['name'] = crt
+		dict['data'] = data
+		data_series.append(dict)
+
+	# relevant assessments
+	if grade == 'all':
+		relevant_assessments = assessments	
+	else:
+		relevant_assessments = sorted(set([row['standard'] for row in mylist if row['grade'] == grade]))
+	
+	return data_series, relevant_assessments
+
 ##################### app #####################
 
 app = Flask(__name__)
@@ -55,6 +82,16 @@ def grade(chartID = 'chart_ID', chart_type = 'line', chart_height = 500, crt_gro
 	yAxis = {"title": {"text": 'Score %'}}
 	return render_template('grade.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis,crt_groups=crt_groups)
 
+# by crt group with grade slicer
+@app.route('/crt_group/<grade>')
+def crtGroup(chartID = 'chart_ID', chart_type = 'line', chart_height = 500, grade = 'all'):	
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = crt_by_grade(grade)[0] 
+	title_text = 'Weekly Assessment Tracking - By CRT Group - Grades: %s' % str(grade)
+	title = {"text": title_text} 
+	xAxis = {"categories": crt_by_grade(grade)[1], "title":{"text":'Assessment'}}
+	yAxis = {"title": {"text": 'Score %'}}
+	return render_template('crt_group.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis,grades=grades)
 
 
 ##################### run #####################
